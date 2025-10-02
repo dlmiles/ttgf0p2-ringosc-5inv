95a113583b65a06ebf4e657dd317e100c243280a
Darryl L. Miles <darryl.miles@darrylmiles.org>
2025-10-02 23:50:00 +0100

    GH180 First Flow Test

--- test/test.py
+++ test/test.py
@@ -1,40 +1,98 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-FileCopyrightText: © 2025 Darryl L. Miles
 # SPDX-License-Identifier: Apache-2.0
 
+import os
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
+
+
+def resolve_GL_TEST():
+    gl_test = False
+    if 'GL_TEST' in os.environ:
+        gl_test = True
+    if 'GATES' in os.environ and os.environ['GATES'] == 'yes':
+        gl_test = True
+    return gl_test
+
+
+GL_TEST = resolve_GL_TEST()
+CI = 'CI' in os.environ and os.environ['CI'] != 'false'
 
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+    await Timer(40, unit='us') # show X
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
+    dut.clk.value = 0
+    dut.ena.value = 0
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await Timer(40, unit='us') # show quiscent
+
+    dut._log.info("Enable")
+    dut.ena.value = 1
+    await Timer(40, unit='us') # show ringosc disabled
+
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())
+    await ClockCycles(dut.clk, 10) # show clock running
+
+    dut._log.info("Reset")
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 10)
+
 
     dut._log.info("Test project behavior")
 
+    await ClockCycles(dut.clk, 10)
+
+    if CI or GL_TEST:
+        return None # disabled as no UNIT_DELAY on IHP130 primitives
+
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in.value = 1
+    dut.uio_in.value = 0
+
+    await ClockCycles(dut.clk, 1)
+    uio_out = dut.uio_out.value
+    uo_out  = dut.uo_out.value
+
+    assert uio_out != 0 and uio_out != 0xff
+    assert uo_out != 0 and uo_out != 0xff
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    MAX_CYCLES = 10000000
+    cycles = 0
+    while cycles < MAX_CYCLES:
+        await Timer(100, unit='ps')
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+        uio_out = dut.uio_out.value
+        uo_out  = dut.uo_out.value
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        if uio_out == 0x00 and uo_out == 0x00:
+            dut._log.info(f"cycles={cycles} on uo_out={uo_out} uio_out={uio_out}")
+            break
+
+        if uio_out == 0xff and uo_out == 0xff:
+            dut._log.info(f"cycles={cycles} on uo_out={uo_out} uio_out={uio_out}")
+            break
+
+        if (cycles % 100000) == 0:
+            dut._log.info(f"cycles={cycles}")
+
+        cycles += 1
+
+    # cycles=3176750
+    assert cycles < MAX_CYCLES, f"MAX_CYCLES={MAX_CYCLES} exceeded"
+
+    # let it run on
+    await Timer(100, unit='ns')
+
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    await Timer(100, unit='ns')
